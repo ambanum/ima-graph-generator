e71d50e886efbecdf95c9092d71040de68cf9129
Martin Ratinaud <martinratinaud@gmail.com>
2022-02-16 15:21:51 +0400

    chore: make command execution async

--- src/common/cmd-utils.ts
+++ src/common/cmd-utils.ts
@@ -1,13 +1,16 @@
 import * as logging from 'common/logging';
 
-import { ExecSyncOptionsWithBufferEncoding, execSync } from 'child_process';
+import { ExecOptionsWithBufferEncoding, exec } from 'child_process';
 
-export const execCmd = (cmd: string, options: ExecSyncOptionsWithBufferEncoding = {}): string => {
+export const execCmd = (
+  cmd: string,
+  options: ExecOptionsWithBufferEncoding = { encoding: 'utf8' }
+): string => {
   logging.debug(`[CMD] ${cmd}`);
   const start = process.hrtime();
 
   // https://stackoverflow.com/questions/63796633/spawnsync-bin-sh-enobufs
-  const result = execSync(cmd, { stdio: 'pipe', ...options });
+  const result = exec(cmd, { ...options });
   const end = process.hrtime(start);
   logging.debug(`[CMD] Command completed in ${Number(end[1] / 1000000).toFixed(0)}ms`);
 
